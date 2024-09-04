586d6a2c6253ad214ca4a30e3229cb77660cbb07
Alena <92329824+poletala@users.noreply.github.com>
2024-09-05 00:35:58 +0500

    vF

--- src/ToggleTheme.tsx
+++ src/ToggleTheme.tsx
@@ -2,14 +2,14 @@ import './App.css'
 import { useTheme } from './useTheme'
 
 
-export const ToggleTheme = () => {
-    const [theme, toggleTheme] = useTheme()
 
-    if (document.querySelector('body')?.classList.contains('light')) {
-        document.querySelector('body')?.classList.remove('light')
+export const ToggleTheme = () => {
+    const {theme, toggleTheme} = useTheme()
+    const bodyE = document.querySelector('body')
+    if (bodyE?.classList.contains('light')) {
+        bodyE?.classList.remove('light')
     } 
-    document.querySelector('body')?.classList.add(`${theme}`)
-    
+    bodyE?.classList.add(`${theme}`)
     return (
         <>
             <button className={`toggle-theme-btn`} type="button" onClick={toggleTheme}></button>
--- src/useTheme.ts
+++ src/useTheme.ts
@@ -2,11 +2,11 @@ import { useLocalStorage } from './UseLocalStorage'
 
 export const useTheme = () => {
     const {value: theme, setKeyValue: setTheme} = useLocalStorage('theme', 'dark');
-    const toggleTheme = () => {
+    const toggleTheme = () =>  {
         const themeNew = theme === 'light' ? 'dark' : 'light'
         setTheme(themeNew);
-        console.log(themeNew)
+        
     }
-    return [theme, toggleTheme]
+    return {theme, toggleTheme}
     
 }
